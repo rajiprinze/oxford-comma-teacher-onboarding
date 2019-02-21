a192c009d07bd66a7ac16cb110c8980f5bb9c49c
Soundarya Rajendran <soundaryarj5@gmail.com>
2019-02-21 02:41:21 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,6 +3,7 @@ def oxford_comma(array)
   if(array.length == 1)
     return array.join(" ")
   elsif (array.length == 2)
+    test << 
     (0..array.length-2).each do |i|
       test << array[i] + ","
     end
