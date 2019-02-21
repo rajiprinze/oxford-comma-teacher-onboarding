bd9c25ed3dddee09f95668e112054682da88d7c6
Soundarya Rajendran <soundaryarj5@gmail.com>
2019-02-21 02:42:32 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
   if(array.length == 1)
     return array.join(" ")
   elsif (array.length == 2)
-    test << array[0] + "and" + array[1]
+    test << array[0] + " and " + array[1]
   else
     (0..array.length-2).each do |i|
       test << array[i] + ","
