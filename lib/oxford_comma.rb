cc35920a067c5e20f3cdd2b184f096c5ee01849d
Soundarya Rajendran <soundaryarj5@gmail.com>
2019-02-21 02:40:22 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,11 +2,12 @@ def oxford_comma(array)
   test = []
   if(array.length == 1)
     return array
-  (0..array.length-2).each do |i|
-    test << array[i] + ","
-  end
-  test << "and"
-  test << array[array.length-1]
+  else
+    (0..array.length-2).each do |i|
+      test << array[i] + ","
+    end
+    test << "and"
+    test << array[array.length-1]
 
   return test.join(" ")
 end
